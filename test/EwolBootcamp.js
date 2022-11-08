6390d87cc6e72072b1fafb2ab96e21c2ed446f37
heidrian-eth <heidrian.eth@gmail.com>
2022-11-08 10:10:51 -0300

    [Bugfix] Wait for transaction buyTokens to be mined

--- test/EwolBootcamp.js
+++ test/EwolBootcamp.js
@@ -1,82 +1,36 @@
 const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const {
   anyValue
 } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const {
   expect
 } = require("chai");
 
-const signers = {};
-let contractFactory;
-let contractInstance;
-let contractAddr;
+let defaultSigner;
+let ewolContractFactory;
+let ewolContractInstance;
+let ewolContractAddr;
 
-describe("EwolBootcamp Test", function () {
+describe("Ewol", function () {
+  it("Should deploy the contract", async function () {
+    [defaultSigner] = await ethers.getSigners();
+    ewolContractFactory = await ethers.getContractFactory("Ewol", defaultSigner);
+    ewolContractInstance = await ewolContractFactory.deploy();
 
-  it("Should deploy the smart contract", async function () {
-    const [
-      deployer,
-      firstUser,
-      secondUser
-    ] = await ethers.getSigners();
-    signers.deployer = deployer;
-    signers.firstUser = firstUser;
-    signers.secondUser = secondUser;
+    await ewolContractInstance.deployed();
+    ewolContractAddr = ewolContractInstance.address;
   });
 
-  describe("Deployment", function () {
-
-    it("Should deploy the smart contract", async function () {
-      contractFactory = await ethers.getContractFactory("Ewol", signers.deployer);
-      contractInstance = await contractFactory.deploy(14);
-      await contractInstance.deployed();
-      contractAddr = contractInstance.address;
-      console.log('Contract Ewol deployed to:', contractAddr);
-    });
-
-    it('Should have an initial state variable value of 14', async function () {
-      const initialStateVarValue = await contractInstance.getVar();
-      expect(initialStateVarValue)
-        .to.equal(14);
-    });
-
-    it('Should have the deployer as contract owner', async function () {
-      const expectOwner = await signers.deployer.getAddress();
-      const contractOwner = await contractInstance.owner();
-      expect(contractOwner)
-        .to.equal(expectOwner);
-    });
+  it("Should set the state var to 5", async function () {
+    const setVarTx = await ewolContractInstance.setVar(5);
+    setVarTx.wait();
   });
 
-  describe("State Var Usage", function () {
-    it("Should set the state var to 100", async function () {
-      const setVarTx = await contractInstance.setVar(100);
-      const setVarReceipt = await setVarTx.wait();
-      console.dir({
-        setVarTx,
-        setVarReceipt
-      }, {
-        depth: null
-      });
-    });
-
-    it("Should confirm the state var is 100", async function () {
-      const newStateVarValue = await contractInstance.getVar();
-      expect(newStateVarValue)
-        .to.equal(100);
-    });
-
-    it("Should not be able to set the state var to 101", async function () {
-      const failedTooLargeSetVarTx = contractInstance.setVar(101);
-      expect(failedTooLargeSetVarTx)
-        .to.be.revertedWith("Var value can't be larger than 100");
-    });
-
-    it("Should not be able to set the state var if not the owner", async function () {
-      const contractInstanceForFirstUser = await contractInstance.connect(signers.firstUser);
-
-      const failedNotOwnerSetVarTx = contractInstanceForFirstUser.setVar(10);
-      expect(failedNotOwnerSetVarTx)
-        .to.be.revertedWith("Ownable: caller is not the owner");
-    });
+  it("Should check that the var value is 5", async function () {
+    expect(await ewolContractInstance.getVar())
+      .to.equal(5);
   });
 });
